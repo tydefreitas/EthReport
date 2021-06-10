fcceffc84a19ff87c14712d51f6697cd6b3f6df6
tydefreitas <tydefreitas@gmail.com>
2021-06-09 23:28:28 -0400

    Began converting from csv to sql for db got primary key atm, old csv code currently commented out

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import chromedriver_binary
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import sqlite3
+
+connection = sqlite3.connect('eth_tracking.db')
 
 driver = webdriver.Chrome()
 driver.get('https://ethermine.org/miners/4dc05FCD625552d9Bc396ac9F0BD8923f13E0d8C/payouts')
@@ -20,11 +23,24 @@ f = open(filename, 'a')
 
 today = datetime.now()
 
-#weeeeeeee
+date = today.strftime("%m/%d/%Y")
+
+###sql stuff hopefully###
+cursor = connection.cursor()
+
+# create stores table
+
+command1 = """CREATE TABLE IF NOT EXISTS
+track(track_id TEXT PRIMARY KEY, payouts TEXT, total_eth INTEGER, till_one_eth INTEGER, eth_price INTEGER, profit_usd INTEGER, profit_cad INTEGER)"""
+
+cursor.execute(command1)
+
+###sql stuff hopefully###
 
 headers = 'Date: {} \n'.format(today)
-
+"""
 f.write(headers)
+
 try:
     for x in range(1,100):
         date = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='app']/main/div/div[2]/div/div[2]/div[3]/div/div[2]/div/div/table/tbody/tr[{}]/td[1]".format(x)))).text
@@ -63,5 +79,12 @@ except selenium.common.exceptions.TimeoutException:
     f.write(' '+ '\n')
     f.write(' '+ '\n')
     f.close()
+    """
+cursor.execute("INSERT INTO track VALUES (" + date + ", '31. May 2021 10:43,0.01112, 22. May 2021 19:32,0.01855, 08. May 2021 19:22,0.01322, 24. Apr. 2021 19:11,0.01527, 10. Apr. 2021 15:18,0.02196, 25. Mar. 2021 07:58,0.02554, 11. Mar. 2021 06:16,0.02107', 0.12673, 0.87327, 2571.32, 325, 394)")
 
-    driver.quit()
+cursor.execute("SELECT * FROM track")
+
+results = cursor.fetchall()
+print(results)  
+
+driver.quit()
